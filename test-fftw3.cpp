ea58c29d2fdab81f9b71056a5079b5cb0cb25b5a
Massimo Marchi <mm148881@gmail.com>
2018-12-05 17:35:06 +0100

    test fftw3-mpi

--- test-fftw3.cpp
+++ test-fftw3.cpp
@@ -9,23 +9,29 @@
 #include <iostream>
 #include <random>
 
-#define OMPI_SKIP_MPICXX
-#include <mpi.h>
+
+//#include <mpi.h>
 #include <fftw3-mpi.h>
 using namespace std;
+double Wtime(){
+	timeval tim;
+	double temp=tim.tv_sec+(tim.tv_usec/1000000.0);
+	return temp;
+	}
 
-double my_func(int i,int j, int k){
-	static std::default_random_engine generator;
-	static std::uniform_real_distribution<double> distribution(0.0,1.0);
-	return distribution(generator);;
-}
+
 int main(int argc, char **argv)
 {
-    const ptrdiff_t L =128, M = 128, N = 128;
+	std::default_random_engine generator;
+	std::uniform_real_distribution<double> distribution(0.0,1.0);
+
+
+    const ptrdiff_t L =256, M = 256, N = 256;
     fftw_plan plan;
     double *rin;
     fftw_complex *cout;
     ptrdiff_t alloc_local, local_n0, local_0_start, i, j, k;
+
 
     MPI_Init(&argc, &argv);
     fftw_mpi_init();
@@ -39,15 +45,24 @@ int main(int argc, char **argv)
     /* create plan for out-of-place r2c DFT */
     plan = fftw_mpi_plan_dft_r2c_3d(L, M, N, rin, cout, MPI_COMM_WORLD,
                                     FFTW_MEASURE);
-
+    double time0=MPI_Wtime();
     /* initialize rin to some function my_func(x,y,z) */
     for (i = 0; i < local_n0; ++i)
        for (j = 0; j < M; ++j)
          for (k = 0; k < N; ++k)
-       rin[(i*M + j) * (2*(N/2+1)) + k] = my_func(local_0_start+i, j, k);
+       rin[(i*M + j) * (2*(N/2+1)) + k] = distribution(generator);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
+    if(rank == 2) {
+    	std::cout << local_0_start <<endl;
+    }
     /* compute transforms as many times as desired */
     fftw_execute(plan);
+
+    double time1=MPI_Wtime();
+
+    if(rank==0) std::cout << time1-time0 << std::endl;
 
     fftw_destroy_plan(plan);
 
